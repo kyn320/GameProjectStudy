2c90afb36c98addc4801e57583956cead9c2cab1
kyn320 <kyn320@naver.com>
2018-05-23 11:08:58 +0900

    init commit

---
+++ Assets/CameraFollower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 margin;
+
+    Transform tr;
+
+    private void Awake()
+    {
+        tr = GetComponent<Transform>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (target == null)
+            return;
+
+        tr.position = Vector3.Lerp(tr.position, target.position + margin, Time.deltaTime * 10f);
+    }
+
+
+}
